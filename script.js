9487ba765cf8d0f92d3f5a14c81682f60f1de469
JamesRileydev <jamesriley.dev@gmail.com>
2018-08-21 19:43:31 -0400

    adding xml code to script file

---
+++ script.js
@@ -0,0 +1,22 @@
+window.onload=function(){
+
+var btn = document.getElementById("btn");
+
+  //listen for clicks
+btn.addEventListener("click", function() {
+  //make the request
+var XHR = new XMLHttpRequest();
+
+XHR.onreadystatechange = function() {
+  if(XHR.readyState == 4 && XHR.status ==200 ) {
+    console.log(XHR.responseText);
+  }
+}
+
+XHR.open("GET", "https://dog.ceo/api/breeds/image/random");
+XHR.send();
+
+});
+
+
+}
