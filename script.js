11b590db4ef057a6010a2552da782e0b51dd12b1
JamesRileydev <jamesriley.dev@gmail.com>
2018-09-03 15:39:12 -0400

    Minor changes

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-window.onload=function(){
+window.onload = function () {
 
-var btn = document.querySelector("#btn");
-var img = document.querySelector("#photo");
+  var btn = document.querySelector("#btn");
+  var img = document.querySelector("#photo");
 
   //listen for clicks
-btn.addEventListener("click", function() {
-  //make the request
-var XHR = new XMLHttpRequest();
+  btn.addEventListener("click", function () {
+    //make the request
+    var XHR = new XMLHttpRequest();
 
-XHR.onreadystatechange = function() {
-  if(XHR.readyState == 4 && XHR.status ==200 ) {
-    var url = JSON.parse(XHR.responseText).message;
-    // var data = JSON.parse(XHR.responseText);
-    // console.log(data.message);
-    img.src = url;
-  }
-}
-XHR.open("GET", "https://dog.ceo/api/breeds/image/random");
-XHR.send();
-});
+    XHR.onreadystatechange = function () {
+      if (XHR.readyState == 4 && XHR.status == 200) {
+        var url = JSON.parse(XHR.responseText).message;
+        // var data = JSON.parse(XHR.responseText);
+        // console.log(data.message);
+        img.src = url;
+      }
+    }
+    XHR.open("GET", "https://dog.ceo/api/breeds/image/random");
+    XHR.send();
+  });
 
 }
